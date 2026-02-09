4e081ac02b4653040051b0cdd151a58ccdec6097
Айза <ayza.oyun@yandex.ru>
2026-02-09 16:14:59 +0300

    обновление prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-
 set -e
 
-# ждем PostgreSQL
+# Ждем PostgreSQL
 sleep 2
 
-
-# создаем бд, если не сущ-ет
-psql 'postgresql://validator:val1dat0r@localhost:5432/postgres' -c "CREATE DATABASE \"project-sem-1\";" 2>/dev/null || echo "Database already exists or can't create"
-
-# создаем табл.
 psql 'postgresql://validator:val1dat0r@localhost:5432/project-sem-1' -c "
 CREATE TABLE IF NOT EXISTS prices (
     id SERIAL PRIMARY KEY,
@@ -17,4 +11,4 @@ CREATE TABLE IF NOT EXISTS prices (
     category TEXT NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     create_date TIMESTAMP NOT NULL
-);" 2>/dev/null || echo "Table checked or error"
+);" 2>/dev/null || echo "Table checked"
