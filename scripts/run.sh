5ed289bcc6f1c15cab4df63cca2dbb26897dbfb2
Айза <ayza.oyun@yandex.ru>
2026-02-09 15:14:06 +0300

    update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 set -e
 
-# запускаем сервер
-go run main.go &
-SERVER_PID=$!
-
-# Ждем запуска
+nohup go run main.go &
 sleep 3
-
-# проверяем что сервер запущен
-if ps -p $SERVER_PID > /dev/null; then
-    echo "Server started with PID: $SERVER_PID"
-    echo $SERVER_PID > /tmp/server.pid
-else
-    echo "Failed to start server"
-    exit 1
-fi
-    
